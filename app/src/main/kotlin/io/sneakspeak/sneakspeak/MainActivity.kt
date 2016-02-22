0a6ae3ef92693fb494b769f4c2a7f40d4bbdd4ab
Iiro Nykänen <iaj.nykanen@gmail.com>
2016-02-22 11:47:03 +0200

    Removed drawerlayout as obsolete.

--- app/src/main/kotlin/io/sneakspeak/sneakspeak/MainActivity.kt
+++ app/src/main/kotlin/io/sneakspeak/sneakspeak/MainActivity.kt
@@ -1,44 +1,23 @@
 package io.sneakspeak.sneakspeak
 
-import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.MenuItem
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import io.sneakspeak.sneakspeak.R.layout.app_bar_main
 import kotlinx.android.synthetic.main.app_bar_main.*
+
 import kotlinx.android.synthetic.main.content_main.*
-import okhttp3.MediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
-import org.jetbrains.anko.async
 import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.toast
-import org.json.JSONObject
-import kotlin.jvm.*
 
 
-class MainActivity : AppCompatActivity(),
-        NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
-
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(app_bar_main)
         toolbar.subtitle = "Shhh"
         setSupportActionBar(toolbar)
 
-        val toggle = ActionBarDrawerToggle(
-                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawerLayout.setDrawerListener(toggle)
-        toggle.syncState()
-
-        navigationView.setNavigationItemSelectedListener(this)
         connectButton.setOnClickListener(this)
     }
 
@@ -62,37 +41,5 @@ class MainActivity : AppCompatActivity(),
 
         // val service = Intent(this, javaClass<SneakInstanceIDService>())
         startService(intentFor<SneakInstanceIDService>())
-    }
-
-    override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        val id = item.itemId
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
     }
 }
